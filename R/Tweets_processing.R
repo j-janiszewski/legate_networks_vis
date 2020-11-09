3670eeb655503f6e2eada333be514757231af00d
Rascoon <37154224+Rascoon@users.noreply.github.com>
2020-11-09 22:38:55 +0100

    typo fix

--- R/Tweets_processing.R
+++ R/Tweets_processing.R
@@ -8,6 +8,6 @@
 #' @return tweets between given dates
 #' @export
 filter_by_date <- function(tweets, start, end) {
-  x <- dplyr::filter(dplyr::between(as.Date(created_at), as.Date(start), as.Date(end)))
+  x <- dplyr::filter(tweets,dplyr::between(as.Date(tweets$created_at), as.Date(start), as.Date(end)))
   return(x)
 }
